8aaf3d2417c91d107af793d176ad9ec717c5ae5d
Kuinox <nvandeginste@gmail.com>
2020-12-01 01:39:12 +0100

    Repo init.

---
+++ TestRepro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestRepro
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			await Task.Delay(10, new System.Threading.CancellationToken()); //no warn.
+			await Task.Delay(10); //warn
+		}
+	}
+}
